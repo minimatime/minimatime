d8c8c89d92b40077ab18253fa804544450cacd99
minimatime <nvalluri3@gatech.edu>
2024-12-14 04:22:13 -0500

    added updated pictures, edited styles.css, animtion for drop down, feeling very sober

--- website/src/App.js
+++ website/src/App.js
@@ -1,24 +1,24 @@
+import SvgIcon from '@mui/icons-material/ArrowDownward';
 import './App.css';
 import './styles.css'
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 function App() {
   return (
     <div className="App">
 
+<div className="heading"><h1>Hi, this is Natasha</h1></div>
       <div className="header">
-        <div className="header-image-carousel">
-          <img src = {"./images/conference-pic.JPG"} alt="fall 24 sigdoc conference" />
-            <img src = {"./images/concert-pic.JPG"} alt="fall 24 showcase concert" />
-            {/* <img src = {"./images/creatives-pic.JPG"} alt="fall 24 creatives photoshoot" /> */}
-          </div>
         
         <div className="about">
           <div id="me">
-            <h3>Hi, this is Natasha</h3>
+            <h3>About</h3>
             <p>My name is Natasha and I'm a developer and designer studying computer science and interaction design
                in a unique major called Computational Media at Georgia Tech. 
                The roots of my passion stem from my curiosity and love for storytelling, community building, 
-               and interactive experiences. Born and raised in Hyderabad, India, studying in Atlanta, Georgia and 
+               and interactive experiences.
+               <br></br><br></br>
+               Born and raised in Hyderabad, India, studying in Atlanta, Georgia and 
                with work experience in different parts of the globe through the Global Citizen Year Academy, Kulfi Beauty, 
                AIonSpectrum and WiTech, I believe in creating local change for global problems through technology, community, 
                and design.
@@ -28,22 +28,38 @@ function App() {
             <h3>Connect</h3>
             <a href = "https://www.instagram.com/nevermindnatasha/" target= "_blank"><p>Instagram</p></a>
             <a href = "https://www.linkedin.com/in/natashavalluri/" target ="_blank"><p>LinkedIn</p></a>
-            <p>Email</p>
-            <p>Discord</p>
+            <a href = "mailto: nvalluri3@gatech.edu" target = "_blank"><p>Email</p></a>
           </div>
           <div id="recent">
             <h3>Currently...</h3>
-            <p>Working on a capstone final project</p>
+            <p>finishing up my capstone work</p>
+            <p>getting back into building personal projects</p>
+            <p>streaming albums everyday</p>
             <h3>Previously...</h3>
-            <p>researching design collaboration</p>
+            <p>presenting research on design collaboration</p>
+            <p>building an analytics dashboard for a georgia non-profit</p>
+            <p>peforming for the rock and pop showcase with my band</p>
           </div>
         </div>
 
+        <div className="header-image-carousel">
+          <img src = {"./images/nyc-tv.png"} alt="Net art piece from NYC" />
+            <img src = {"./images/me-studying.png"} alt="Natasha locked in at the library" />
+            <img src= {"/images/nyc-flowers.png"} alt="Flowers from a market in NYC" />
+          </div>
 
       </div>
+      
+      <div className="bounce">
+          <SvgIcon>
+            <ArrowDownwardIcon/> 
+          </SvgIcon>
+      </div>
 
+      <div className="heading"><h1>Projects</h1></div>
       {/* personal projects section */}
       <div className="project">
+        
         <div className="description">
           <h3>Cat Jam</h3>
           <p>Engineered and design a cat-inspired music making web application
